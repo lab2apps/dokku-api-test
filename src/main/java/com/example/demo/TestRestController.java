6863b8325e911de4bf35c10457731d5bad14dd6b
mmazurkevich <mazumisha@gmail.com>
2018-11-08 19:18:57 +0300

    Check DB connection

--- src/main/java/com/example/demo/TestRestController.java
+++ src/main/java/com/example/demo/TestRestController.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestRestController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
     @GetMapping("/lol")
-    public Greeting lol(){
-        return new Greeting(34, "dfdf");
+    public String lol(){
+        return dbUrl;
     }
 }
